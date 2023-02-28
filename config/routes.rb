648cd517e836b0ba5ab2df9252dc2d7839096442
David <david.cabuay@gmail.com>
2023-02-28 13:49:23 -0800

    Prepare for render.com

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :listings, only: [:index, :create, :show]
   end
   
+  get '*path', to: "static_pages#frontend_index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
