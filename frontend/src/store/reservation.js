2ca7021127c1fb145fe705dc56347965f196c553
David <david.cabuay@gmail.com>
2023-03-02 11:46:42 -0800

    Added store/reservation.js

--- frontend/src/store/reservation.js
+++ frontend/src/store/reservation.js
@@ -2,63 +2,104 @@ import csrfFetch from './csrf';
 
 export const SET_RESERVATION = 'reservation/SET_RESERVATION'
 export const SET_RESERVATIONS = 'reservation/SET_RESERVATION'
+export const REMOVE_RESERVATION = 'reservation/REMOVE_RESERVATION'
 
-const setListing = (listing) => {
+const setReservation = (reservation) => {
     return {
-        type: SET_LISTING,
-        listing
+        type: SET_RESERVATION,
+        reservation
     }
 }
 
-const setListings = (listings) => {
+const setReservations = (reservations) => {
     return {
-        type: SET_LISTINGS,
-        listings
+        type: SET_RESERVATIONS,
+        reservations
     }
 }
 
-export const getListing = (listingId) => (state) => {
-    return state.listings ? state.listings[listingId] : null
+const removeReservation = (reservationId) => {
+    return {
+        type: REMOVE_RESERVATION,
+        reservationId
+    }
 }
 
-export const getListings = (state) => {
-    // console.log(state, 'getlistings')
-    return state.listings ? Object.values(state.listings) : []
+export const getReservation = (reservationId) => (state) => {
+    return state.reservations ? state.reservations[reservationId] : null
 }
 
-export const fetchListings = () => async(dispatch)=> {
-    const response = await csrfFetch(`/api/listings`)
+export const getReservations = (state) => {
+
+    return state.reservations ? Object.values(state.reservations) : []
+}
+
+export const fetchReservations = () => async(dispatch)=> {
+    const response = await csrfFetch(`/api/reservations`)
     if (response.ok){
-        const listings = await response.json();
-        dispatch(setListings(listings))
+        const reservations = await response.json();
+        dispatch(setReservations(reservations))
     }
 }
 
-export const fetchListing = (listingId) => async(dispatch)=> {
-    const response = await csrfFetch(`/api/listings/${listingId}`)
+export const fetchReservation = (reservationId) => async(dispatch)=> {
+    const response = await csrfFetch(`/api/reservations/${reservationId}`)
     if (response.ok){
-        const listing = await response.json();
-        dispatch(setListing(listing))
+        const reservation = await response.json();
+        dispatch(setReservation(reservation))
+    }
+}
+export const createReservation = (reservation) => async(dispatch) => {
+    const response = await fetch(`/api/ponseervations`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(ponseervation)
+    })
+    if (response.ok){
+        const newreservation = await response.json();
+        dispatch(setReservation(newreservation))
     }
 }
 
+export const updateReservation = (reservation) => async(dispatch) => {
+    const response = await fetch(`/api/reservations/${reservation.id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(reservation)
+    })
+    if (response.ok){
+        const editreservation = await response.json();
+        dispatch(setReservation(editreservation))
+    }
+}
 
+export const deleteReservation = (reservationId) => async(dispatch) => {
+    const response = await fetch(`/api/reservations/${reservationId}`, {
+        method: "DELETE",
+    })
+    if (response.ok){
+        dispatch(removeReservation(reservationId))
+    }
+}
 // const initialState = { 
-//     user: JSON.parse(sessionStorage.getItem("listings"))
+//     user: JSON.parse(sessionStorage.getItem("reservations"))
 // };
 
-const listingsReducer = (state= {}, action) =>{
+const reservationsReducer = (state= {}, action) =>{
+    const newState = {...state};
     switch (action.type) {
-        case SET_LISTINGS:
-            return action.listings;
-        case SET_LISTING:
-            const newState = {...state};
-            const listing = action.listing;
-            newState[listing.id] = listing;
+        case SET_RESERVATIONS:
+            return action.reservations;
+        case SET_RESERVATION:
+            const reservation = action.reservation;
+            newState[reservation.id] = reservation;
             return newState;
+        case REMOVE_RESERVATION:
+            delete newState[action.reservationId]
+            return newState
         default:
             return state;
     }
 }
 
-export default listingsReducer
+export default reservationsReducer
